bcf36d8f207b04b6808fe88895423a7c264a004d
iam-shanmukha <vishnushanmukha@gmail.com>
2020-12-05 00:01:34 +0530

    minor changes

--- organiser.py
+++ organiser.py
@@ -9,16 +9,17 @@ twitter: @iam_shanmukha
 import os
 import shutil
 
-doc_extensions=[".txt",".docx",".doc",".xls",".xls"]
+doc_extensions=[".txt",".docx",".doc",".xls",".xls",".csv"]
 compress_ext =[".zip",".rar",".zst",".gz",".xz"]
 vids_ext=[".mp4",".avi",".mkv"]
 pic_ext=[".png",".jpg",".jpeg"]
+softwares_ext = [".AppImage",".deb"]
 
 #list present working directory files and folders
 #ls = files+folders
 ls=os.listdir()
 def create_dirs():
-	directories = ["Pdfs", "Pictures", "Videos", "compressed_Files", "Documents", "Py_files"]
+	directories = ["Pdfs", "Pictures", "Videos", "compressed_Files", "Documents", "Py_files", "softwares"]
 	for directory in directories:	
 		if directory in ls:
 			print(f"{directory} directory exists...skipping ")
@@ -26,24 +27,33 @@ def create_dirs():
 			print(f"Creating {directory} Directory")
 			os.mkdir(directory)
 create_dirs()
+
 for file in ls:
-	if file.endswith(".pdf"):
-		shutil.move(file, "Pdfs")
-		print(f"{file} files moved successfully")
-	elif file.endswith(tuple(pic_ext)):
-		shutil.move(file, "Pictures")
-		print(f"{file} files moved successfully")
-	elif file.endswith(tuple(vids_ext)):
-		shutil.move(file, "Videos")
-		print(f"{file} files moved successfully")
-	elif file.endswith(tuple(compress_ext)):
-		shutil.move(file, "compressed_Files")
-		print(f"{file} files moved successfully")
-	elif file.endswith(tuple(doc_extensions)):
-		shutil.move(file, "compressed_Files")
-		print(f"{file} files moved successfully")
-	elif file.endswith(".py"):
-		if file == "organiser.py":
-			continue
-		shutil.move(file, "Py_files")
-		print(f"{file} files moved successfully")
+	try:
+
+		if file.endswith(".pdf"):
+			shutil.move(file, "Pdfs")
+			print(f"{file} files moved successfully")
+		elif file.endswith(tuple(pic_ext)):
+			shutil.move(file, "Pictures")
+			print(f"{file} files moved successfully")
+		elif file.endswith(tuple(vids_ext)):
+			shutil.move(file, "Videos")
+			print(f"{file} files moved successfully")
+		elif file.endswith(tuple(compress_ext)):
+			shutil.move(file, "compressed_Files")
+			print(f"{file} files moved successfully")
+		elif file.endswith(tuple(doc_extensions)):
+			shutil.move(file, "compressed_Files")
+			print(f"{file} files moved successfully")
+		elif file.endswith(tuple(softwares_ext)):
+			shutil.move(file, "softwares")
+			print(f"{file} files moved successfully")
+		elif file.endswith(".py"):
+			if file == "organiser.py":
+				continue
+			shutil.move(file, "Py_files")
+			print(f"{file} files moved successfully")
+	except shutil.Error as E:
+		print("ERROR",E)
+		continue
